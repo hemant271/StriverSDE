c784d3e3fc5d297c8682d308b1a1d92cacf62cb6
Hemant Patidar <patidarhemant27@gmail.com>
2023-06-15 01:34:23 +0530

    Reverse Linked List

--- StriverSDEPratice/LinkedList/LinkedList.cs
+++ StriverSDEPratice/LinkedList/LinkedList.cs
@@ -6,8 +6,69 @@ using System.Threading.Tasks;
 
 namespace StriverSDEPratice.LinkedList
 {
-    internal class LinkedList
+    public class Node<T>
     {
-
+        public T Value;
+        public Node<T>? Next;
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
     }
+
+    public class LinkedListOps
+    {
+        //Add node
+        public void Add(int data, Node<int> head)
+        {
+            Node<int> node = new Node<int>(data);
+            
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node<int> curr = head;
+                while(curr.Next != null)
+                {
+                    curr = curr.Next;
+                }
+                curr.Next = node;
+            }
+        }
+
+        //Display list
+        public void display(Node<int> head)
+        {
+            if (head == null)
+                Console.Write("List is empty");
+            while(head != null)
+            {
+                Console.Write(head.Value + "-->");
+                head = head.Next;
+            }
+        }
+
+        //Reverse List
+        public Node<int> ReverseList(Node<int> head)
+        {
+            Node<int> curr = head;
+            Node<int> prev = null, next = null;
+
+            while(curr != null)
+            {
+                next = curr.Next;
+                curr.Next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            return prev;
+
+        }
+        
+    }
+
 }
--- StriverSDEPratice/Program.cs
+++ StriverSDEPratice/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using StriverSDEPratice.LinkedList;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        LinkedListOps list = new LinkedListOps();
+        Node<int> head = new Node<int>(1);
+        list.Add(2, head);
+        list.Add(3, head);
+        head = list.ReverseList(head);
+        list.display(head);
+        Console.ReadKey();
+    }
+}
